6dc17eb05f2934d1f71c0fbcddf593d2ad30f690
svanka024 <1196688@student.roc-nijmegen.nl>
2022-11-04 12:45:26 +0100

    commit

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ function myFunction() {
     document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselbutton === "next" ? 1 : -1;
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]");
+
+        const activeslide = slides.querySelector("[data-active]");
+        let newindex = [...slides.children].indexOf(activeslide) + offset;
+        if (newindex < 0) newindex = slides.children.length - 1;
+        if (newindex >= slides.children.length) newindex = 0;
+
+        slides.children[newindex].dataset.active = true;
+        delete activeslide.dataset.active;
+    });
+});
