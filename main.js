16ba565bbcd05d31f591754bec6b467147d17e2f
svanka024 <1196688@student.roc-nijmegen.nl>
2022-11-08 13:14:06 +0100

    commit

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        const offset = button.dataset.carouselbutton === "next" ? 1 : -1;
-        const slides = button
+            const offset = button.dataset.carouselbutton === "next" ? 1 : -1;
+            const slides = button
             .closest("[data-carousel]")
             .querySelector("[data-slides]");
 
