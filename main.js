36b423888989c60176515d0beb4191246ff93c53
svanka024 <1196688@student.roc-nijmegen.nl>
2022-11-11 11:10:42 +0100

    img slider

--- main.js
+++ main.js
@@ -8,23 +8,34 @@ function myFunction() {
     document.getElementById("myBar").style.width = scrolled + "%";
 }
 
-
+//img slider start
 const buttons = document.querySelectorAll("[data-carousel-button]");
-
 buttons.forEach((button) => {
 
     button.addEventListener("click", () => {
-            const offset = button.dataset.carouselbutton === "next" ? 1 : +1;
+            const way = button.getAttribute("data-carousel-button");
             const slides = button
             .closest("[data-carousel]")
             .querySelector("[data-slides]");
 
+        //active slide
         const activeslide = slides.querySelector("[data-active]");
-        let newindex = [...slides.children].indexOf(activeslide) + offset;
-        if (newindex < 0) newindex = slides.children.length - 1;
-        if (newindex >= slides.children.length) newindex = 0;
+        let activeIndex = [...slides.children].indexOf(activeslide);
 
+        //als de way variable gelijk is aan prev doe dan -1
+        let newindex = activeIndex + 1;
+        if(way === 'prev')
+        {
+            newindex = activeIndex - 1
+        }
+
+        //begin slider bepalen en als die bij de laatste is begint hij weer bij 0
+        if (newindex >= slides.children.length) newindex = 0;
+        if (newindex < 0) newindex = (slides.children.length - 1);
+
+        //de active slide
         slides.children[newindex].dataset.active = true;
         delete activeslide.dataset.active;
     });
 });
+//img slider end
